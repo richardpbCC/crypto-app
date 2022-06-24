fc0fd24978e8b6baacb29009ccfa10011df15b71
richardpb <rpbeavis@gmail.com>
2022-06-24 20:11:24 +0900

    add: Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptoverse App</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
