2d510831047cd3ea7acea19374bf577b56e87380
richardpb <rpbeavis@gmail.com>
2022-10-04 22:29:25 +0900

    style: add article details including image, provider and time published

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,6 +6,7 @@ import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
+const defaultImageUrl = "";
 
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
@@ -21,20 +22,44 @@ const News = ({ simplified }) => {
     <Row gutter={[24, 24]}>
       {cryptoNews.value.map((article, index) => (
         <Col xs={24} sm={12} lg={8} key={index}>
-          <Card 
-            className="news-card"
-            hoverable
-            >
-              <a href={article.url} target ="_blank" rel="noreferrer">
-                <div classname="news-image-container">
-                  <Title className="news-title" level={4}>{article.name}</Title>
+          <Card className="news-card" hoverable>
+            <a href={article.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {article.name}
+                </Title>
+                <img
+                  style={{maxWidth: "200px", maxHeight: "100px"}}
+                  src={article?.image?.thumbnail?.contentUrl || defaultImageUrl}
+                  alt="news thumbnail image"
+                />
+              </div>
+              <p>
+                {article.description > 100
+                  ? `${article.description.substring(0, 100)}... `
+                  : article.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      article.provider[0]?.image?.thumbnail?.contentUrl ||
+                      defaultImageUrl
+                    }
+                    alt="news provider image"
+                  />
+                  <Text classname="provider-name">{article.provider[0]?.name}</Text>
                 </div>
-              </a>
+                <Text>
+                  {moment(article.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
           </Card>
         </Col>
       ))}
     </Row>
-  )
+  );
 };
 
 export default News;
