75272563d069d76593523f332b985d4e213abfec
richardpb <rpbeavis@gmail.com>
2022-10-04 21:41:35 +0900

    feat: integrate cryptonews api and update store and redux services

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
-  return (
-    <div>
-      News
-    </div>
-  )
-}
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 10 : 100,
+  });
+
+  console.log(cryptoNews);
+
+  return <div>News</div>;
+};
 
 export default News;
