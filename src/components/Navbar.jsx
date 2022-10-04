2ecd581a0b7d91d49b326fc6b694dc20612c3022
richardpb <rpbeavis@gmail.com>
2022-10-04 21:56:47 +0900

    feat: display latest crypto news articles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/">Cryptoverse App</Link>
+          <Link to="/">Cryptoverse</Link>
         </Typography.Title>
         {/* <Button className="menu-control-container">
 
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,12 +10,31 @@ const { Option } = Select;
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: "Cryptocurrency",
-    count: simplified ? 10 : 100,
+    count: simplified ? 6 : 12,
   });
 
-  console.log(cryptoNews);
+  if (!cryptoNews?.value) {
+    return "Loading...";
+  }
 
-  return <div>News</div>;
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((article, index) => (
+        <Col xs={24} sm={12} lg={8} key={index}>
+          <Card 
+            className="news-card"
+            hoverable
+            >
+              <a href={article.url} target ="_blank" rel="noreferrer">
+                <div classname="news-image-container">
+                  <Title className="news-title" level={4}>{article.name}</Title>
+                </div>
+              </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  )
 };
 
 export default News;
